778ef97e89c86334fa11b0d0f6a682ead3564aa7
Jonas Gösgens <jonas.goesgens@rwth-aachen.de>
2025-12-15 18:32:43 +0100

    Setup ArgRec Experiments

---
+++ submit_jobs_6b.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name=arg_rec_sift
+#SBATCH --output=output/stdout/job_%A_%a.out
+#SBATCH --error=output/stderr/job_%A_%a.err
+#SBATCH --array=3,5
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=160G
+#SBATCH --gpus=0
+
+# Benchmarks
+input_dir="./benchmark"
+input_files=("arg_rec_paper_table1.txt")
+
+file_index=0
+line_index=$((SLURM_ARRAY_TASK_ID))
+
+input_file="${input_files[$file_index]}"
+
+temp_file=$(mktemp "/tmp/${input_file%.txt}_table1_line$(printf "%02d" $line_index).XXXXXX")
+trap 'rm -f "$temp_file"' EXIT
+
+sed -n "$((line_index + 1))p" "$input_dir/$input_file" > "$temp_file"
+
+apptainer run --bind .:/sift --bind /tmp:/tmp ../sift-container.sif /sift/main.py -br "$temp_file" -p 10
+
+exit_status=$?
+
+if [ $exit_status -ne 0 ]; then
+    echo "Error: The command failed with exit status $exit_status." >&2
+fi
+
+exit $exit_status
