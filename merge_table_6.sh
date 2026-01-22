9b5c201d3c89af13a828fbb9f40229ea38cb214a
Jonas Gösgens <jonas.goesgens@rwth-aachen.de>
2026-01-22 15:26:30 +0100

    merges all lines into a single table.

--- merge_table_6.sh
+++ merge_table_6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-output_dir="output/tables"
-result_file="output/tables/arg_rec_paper_table1_line03_table.txt"
+work_dir="output/tables"
+output_file="$work_dir"/arg_rec_paper_table1_table.txt
 
-echo "" > "$result_file"
+max_line_index=14
 
 declare -A max_values
 declare -A table_entries
@@ -21,53 +21,92 @@ indices=("avg_objects_learning" "num_edges_learning" "orig_args" "rec_args" "ext
           "num_edges_verifi" "avg_time_verifi" "success_rate")
 
 units=("" "" "" "" "" "" "" '\\seconds' "" "" '\\seconds' '\\%' )
+num_units=${#units[@]}
 
-total_runs=0
-for input_file in "$output_dir"/arg_rec_paper_table1_line03_run*_table.txt; do
-    total_runs=$(($total_runs + 1))
-    base_name=$(basename "$input_file" _table.txt)
-    source_name="${base_name%_run*}"
+split_lines=("03")
 
-    while IFS= read -r line; do
-        values=($(echo "$line" | awk -F'&' '{for(i=2;i<=NF;i++) print $i}' | tr -d ' '))
+for table_line in "${split_lines[@]}"; do
+    result_file_line="$work_dir"/arg_rec_paper_table1_line"$table_line"_table.txt
+    total_runs=0
+    for input_file in "$work_dir"/arg_rec_paper_table1_line"$table_line"_run*_table.txt; do
+        total_runs=$(($total_runs + 1))
+        base_name=$(basename "$input_file" _table.txt)
+        source_name="${base_name%_run*}"
 
-        for i in "${!indices[@]}"; do
-            key=${indices[$i]}
-            value=$(echo "${values[$i]}" | tr -d '$')
-            value=$(echo "$value" | tr -d "${units[$i]}")
-            echo $value
-            echo ${units[$i]}
-            if [[ $key == orig_args || $key == rec_args || $key == extra_args || $key == max_all_features ]]; then
-                if (( $(echo "${value} > ${max_values[$key]:-0}" ) )); then
-                    max_values[$key]=$value
+        while IFS= read -r line; do
+            values=($(echo "$line" | awk -F'&' '{for(i=2;i<=NF;i++) print $i}' | tr -d ' '))
+
+            for i in "${!indices[@]}"; do
+                key=${indices[$i]}
+                value=$(echo "${values[$i]}" | tr -d '$')
+                value=$(echo "$value" | tr -d "${units[$i]}")
+                #echo $value
+                #echo ${units[$i]}
+                if [[ $key == orig_args || $key == rec_args || $key == extra_args || $key == max_all_features ]]; then
+                    if (( $(echo "${value} > ${max_values[$key]:-0}" ) )); then
+                        max_values[$key]=$value
+                    fi
+                else
+                    max_values[$key]=$(( ${max_values[$key]} + value ))
                 fi
-            else
-                max_values[$key]=$(( ${max_values[$key]} + value ))
-            fi
 
-        done
+            done
 
-    done < "$input_file"
+        done < "$input_file"
+    done
+
+    #total_runs=25
+
+    for key in "${!max_values[@]}"; do
+        if [[ ! "orig_args rec_args extra_args max_all_features success_rate" =~ $key ]]; then
+        max_values[$key]=$(bc <<< "scale=2; ${max_values[$key]} / $total_runs")
+        fi
+    done
+
+    stats_table_out=""
+    for i in "${!indices[@]}"; do
+        key=${indices[$i]}
+        unit=${units[$i]}
+
+        stats_table_out+="&\$${max_values[$key]}$unit\$"
+    done
+    final_output="$(demask_latex "${stats_table_out}")"
+    echo "${final_output}" > "$result_file_line"
+
+    echo "Saved table line'$table_line' as '$result_file_line'."
+
 done
 
-#total_runs=25
+default_line=""
+for i in $(seq 0 $((num_units - 1))); do
+    default_line+="&-${units[i]}"
+done
+default_line="$(demask_latex "${default_line}")"
 
-for key in "${!max_values[@]}"; do
-    if [[ ! "orig_args rec_args extra_args max_all_features success_rate" =~ $key ]]; then
-       max_values[$key]=$(bc <<< "scale=2; ${max_values[$key]} / $total_runs")
+lines=()
+for i in $(seq 0 $max_line_index); do
+    lines[i]=$default_line
+done
+
+for input_file in "$work_dir"/arg_rec_paper_table1_line*_table.txt; do
+    if [[ $input_file =~ arg_rec_paper_table1_line([0-9]{2})_table\.txt ]]; then
+        line_number=${BASH_REMATCH[1]}
+        line_number_int=$((10#$line_number))
+        if (( line_number_int >= 0 && line_number_int <= max_line_index )); then
+            #echo $line_number
+            #echo $line_number_int
+            #echo $(cat "$input_file")
+            lines[line_number_int]=$(cat "$input_file")
+        else
+            echo "Warning: Line number $line_number out of range (00-$max_line_index) for file '$input_file'."
+        fi
     fi
 done
 
-stats_table_out=""
-for i in "${!indices[@]}"; do
-    key=${indices[$i]}
-    unit=${units[$i]}
+{
+    for i in $(seq 0 $max_line_index); do
+        echo "${lines[i]}"
+    done
+} > "$output_file"
 
-    stats_table_out+="&\$${max_values[$key]}$unit\$"
-done
-#final_output=$stats_table_out
-final_output="$(demask_latex "${stats_table_out}")"
-#final_output="${final_output}\\"
-echo "${final_output}" >>"$result_file"
-
-echo "Die Tabelle wurde als '$result_file' gespeichert."
+echo "Merged table saved as '$output_file'."
