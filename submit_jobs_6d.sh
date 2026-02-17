1fdc72c8a9dec0f7de5478caf90c564fc0cf68f5
Jonas Goesgens <jonas.goesgens@rwth-aachen.de>
2026-02-17 10:39:35 +0100

    fixed submit

--- submit_jobs_6d.sh
+++ submit_jobs_6d.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=arg_rec_sift
 #SBATCH --output=output/stdout/job_%A_%a.out
 #SBATCH --error=output/stderr/job_%A_%a.err
-#SBATCH --array=0-12
+#SBATCH --array=0-24
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=160G
 #SBATCH --gpus=0
