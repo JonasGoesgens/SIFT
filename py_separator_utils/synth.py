89cdd170a65378b4804e18bdedbe7392a544dff2
niklas <niklas.jansen1@rwth-aachen.de>
2026-03-16 10:54:08 +0100

    synth

--- py_separator_utils/synth.py
+++ py_separator_utils/synth.py
@@ -1,6 +1,8 @@
 from typing import Dict, Tuple
 import py_separator_utils.py_types as pt
 import py_separator_utils.utils as ut
+import py_separator_utils.synth_dependencies.synthalg as alg
+
 def synth_update_graphs(
     graphs : Dict[int, Tuple[pt.GraphT, pt.NodeT]]
 ) -> Dict[int, Tuple[pt.GraphT, pt.NodeT]]:
@@ -9,6 +11,9 @@ def synth_update_graphs(
         assert(initial_node_id in Graph.nodes())
         print(Graph.nodes[initial_node_id].get(pt.Atom_List_key, dict()))
     #run Synth
+    alg.synth(graphs)
+
+
     print(f"{ut.format_cur_time()}: Updating Graph labels", flush=True)
     #add query values to labels
     return graphs
